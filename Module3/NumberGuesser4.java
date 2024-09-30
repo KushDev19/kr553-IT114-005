da84dd779b967d5c76a8d6a4735bfae527b99e3b
KushRank <kushrank24680@gmail.com>
2024-09-30 18:30:06 -0400

    final commit

--- Module3/NumberGuesser4.java
+++ Module3/NumberGuesser4.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
+//kr553 9/30/2024
+
 public class NumberGuesser4 {
     private int maxLevel = 1;
     private int level = 1;
@@ -68,6 +70,7 @@ public class NumberGuesser4 {
                             this.difficulty = difficulty;
                         } else {
                             this.difficulty = "medium"; // default if not found
+                            this.level = 1;
                         }
                     }
                 }
@@ -86,6 +89,7 @@ public class NumberGuesser4 {
         System.out.println(
                 "I picked a random number between 1-" + (range) + ", let's see if you can guess.");
         return true;
+        //kr553 9/30/24
     }
 
     private void setMaxStrikes() {
@@ -142,6 +146,7 @@ public class NumberGuesser4 {
         }
     }
 
+    //kr553 9/30/24
     private void processGuess(int guess) {
         if (guess < 0) {
             return;
